efd0dbb3c173a027aa58dfc6982c8f3ed2b275b4
isadoraachiroli <161837858+isadoraachiroli@users.noreply.github.com>
2024-10-10 08:36:26 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -2,65 +2,77 @@ const caixaPrincipal = document.querySelector('.caixa-principal');
 const caixaPergunta = document.querySelector('.caixa-pergunta');
 const caixaAlternativa = document.querySelector('.caixa-alternativa');
 const caixaResultado = document.querySelector('.caixa-resultado');
+const textoResultado = document.querySelector('.texto-resultado')
 
-const perguntas = [
-    {
-        enunciado: "Você gosta da ideia de Inteligência Artificial?",
-        alternativas:[
-            {
-            txto:"Sim",
-            afirrmativa:"Afirmativa da alternativa 1"
-            },
-            {
-                texto:"Não",
-                airmativa:"Afirmativa da altrnativa 2"
-            }
+const perguntas = [//abre a lista de objetos (itens)
+    {//abre o item
+        enunciado: "Você gosta da Inteligência Artificial?",
+        alternativas: [{
+            texto: "Sim",
+            afirmação: "Sim, utilizo para questões diárias"
+        },
+        {
+            texto: "Não",
+            afirmação: "Não, a IA não me ajuda em nada"
+        }
         ]
     },
     {
-        enunciado: "Você uaria IA?",
-        alternativas:[
-            {
-            txto:"Sim",
-            alternativa:"Afirmativa da alternatiiva 1"
-            },
-            {
-                texto:"Não",
-                afirmativa:"Afirmativa da altrnativa 2"
-            }
-         ]
+        enunciado: "Você sabe utilizar de maneira correta as IA's?",
+        alternativas: [{
+            texto: "Sim",
+            afirmação: "Sim, ela me ajuda e me da dicas sobre o minhas dúvidas"
+        },
+        {
+            texto:"Não",
+            afirmação:"Não, prefiro tirar dúvidas sem as IA's"
+        }
+        ]
     },
     {
-        enunciado: "Vocẽ acha a IA boa par o mundo?",
-        alternativas:[
-            {
-            texto:"Sim",
-            afirmativa:"Airmativa da alternativa 1"
-            },
-            {
-                texto:"Não",
-                afirmativa:"Afirmativa da alternativa 2"
-            }
-         ]
+        enunciado: "Quando você utiliza as IA's no seu cotidiano?",
+        alternativas: [{
+            texto: "Sim",
+            afirmação: "Sim, utilizo na escola para responder minhas perguntas"
+        },
+        {
+            texto: "Não",
+            afirmação: "Não, nunca uso, prefiro pensar sozinho"
+        }
+        ]
     }
 ]
-
 let posicao = 0;
 let perguntaAtual;
+let resposta = "";
 
-function mostraPergunta(){
+function mostraPergunta() {
+    if (posicao>=perguntas.length){
+        mostraResultado();
+        return;
+    }
     perguntaAtual = perguntas[posicao];
     caixaPergunta.textContent = perguntaAtual.enunciado;
+    caixaAlternativa.textContent = " ";
     mostraAlternativas();
 }
-function mostraAlternativas(){
-    for(const alternativa of perguntaAtual.alternativa){
+function mostraAlternativas() {
+    for (const alternativa of perguntaAtual.alternativas) {
         const botaoAlternativas = document.createElement("button");
         botaoAlternativas.textContent = alternativa.texto;
-        botaoAlternativas.addEventListener("click",function(){
-            posicao++;
-            mostraPergunta();
-        });
+        botaoAlternativas.addEventListener("click",()=> respostasSelecionadas(alternativa));
         caixaAlternativa.appendChild(botaoAlternativas);
     }
 }
+function respostaSelecionadas(opcaoSelecionada){
+    const afirmacoes = opcaoSelecionada.afirmativa;
+    resposta += afirmacoes;
+    posicao++,
+    mostraPergunta();
+}
+function mostraResultado(){
+    caixaPergunta.textContent = "Daqui a 10 anos vamos virar robôs";
+    textoResultado.textContent = respostas;
+    caixaAlternativa.textContent = "";
+}
+mostraPergunta();
